af8ad360ff49aba729fc3f863fa580ef911b46c3
mainecoony <shiroe13@gmail.com>
2025-04-22 14:43:40 -0400

    Making a Window

--- main.py
+++ main.py
@@ -3,19 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
+#    print(f"""Starting Asteroids!
+#Screen width: {SCREEN_WIDTH}
+#Screen height: {SCREEN_HEIGHT}""")
     pygame.init()
-    print(f"""Starting Asteroids!
-Screen width: {SCREEN_WIDTH}
-Screen height: {SCREEN_HEIGHT}""")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, 0)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
