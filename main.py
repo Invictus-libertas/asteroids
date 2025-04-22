63b4628d840b5bb92a0ac4b2c3c95982cf80eeed
mainecoony <shiroe13@gmail.com>
2025-04-22 13:56:47 -0400

    Making a Window

--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    print(f"""Starting Asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}""")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, 0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
